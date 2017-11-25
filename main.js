d9bbe3ca734ebd428ce402b9a480a0b73449081f
Vishesh <visheshsingh16@gmail.com>
2017-11-24 22:01:01 -0500

    Image slider using JavaScript

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ $(window).on('scroll', function(){
 $('.handle').on('click', function(){
     $('nav ul').toggleClass('showing');
 })
+
+//Image slider for Looplap projects page
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+    
+// Image List
+images[0] = "img/looplab1.png";
+images[1] = "img/looplab2.png";
+images[2] = "img/looplab3.png";
+images[3] = "img/looplab4.png";
+
+// Change Image
+function changeImg(){
+    document.slide.src = images[i];
+
+    // Check If Index Is Under Max
+    if(i < images.length - 1){
+    // Add 1 to Index
+    i++; 
+    } else { 
+        // Reset Back To O
+        i = 0;
+    }
+
+    // Run function every x seconds
+    setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
